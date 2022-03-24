acdf11a38eb731b2c0a666e4633358209a65f15f
Karl Barbour <karl.barbour@sage.com>
2022-03-24 12:41:02 +0000

    add selector for region

--- toolbox/output/Toolbox.psm1
+++ toolbox/output/Toolbox.psm1
@@ -516,7 +516,21 @@ function Set-AWSRegion {
     Set-DefaultAWSRegion $Region -Scope Global
   }
   else {
-    Write-Error "Must specify a region"
+    $regions = @(
+      "af-south-1", "ap-east-1", "ap-northeast-1", "ap-northeast-2", "ap-northeast-3", "ap-south-1", "ap-southeast-1", "ap-southeast-2", "ap-southeast-3", "ca-central-1", "eu-central-1", "eu-north-1", "eu-south-1", "eu-west-1", "eu-west-2", "eu-west-3", "me-south-1", "sa-east-1", "us-east-1", "us-east-2", "us-west-1", "us-west-2"
+    )
+    $regions = $regions | Sort-Object
+
+    # Iterate through profiles and display
+    for ($i = 0; $i -lt $regions.count; $i++) {
+      Write-Host "$($i): $($regions[$i])"
+    }
+
+    # Ask for selection and set as env var
+    $selection = Read-Host "Choose a region"
+    $ENV:AWS_DEFAULT_REGION = $($regions[$selection])
+    Set-DefaultAWSRegion $($regions[$selection]) -Scope Global
+    Write-Host "Selected $($ENV:AWS_DEFAULT_REGION)"
   }
 }
 
